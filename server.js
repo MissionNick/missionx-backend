343784b9fea4c3352f52e4306d2a1512ff50e9e5
Brandon Trump <brandontrump101@gmail.com>
2022-07-10 01:26:42 +1200

    added server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const { port } = require("./config/index");
+
+// Middleware
+const { connect } = require("./db");
+
+// Routers to be used
+const { studentsRouter } = require("./routes/students.routes");
+
+// Register Routers
+app.use("/api/students", studentsRouter);
+
+// 404 error
+app.all("*", (req, res, next) => {
+  const err = new HttpException(404, `Endpoint ${req.url} Not Found`);
+  next(err);
+});
+
+const startServer = async () => {
+  try {
+    await connect();
+    app.listen(port, () => console.log(`Server running on ${port}`));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+startServer();
