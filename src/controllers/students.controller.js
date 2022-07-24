18596d03cd9728c0dc57fc18f160f945d79e589f
Brandon Trump <brandontrump101@gmail.com>
2022-07-25 05:32:35 +1200

    adjusted students controller

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -1,12 +1,12 @@
 const connection = require("../db/db");
 
 const getAllProfiles = async (req, res) => {
-  console.log("Received a GET request to api/students/ for student profiles");
-
+  console.log(`Received a GET request to api/students/ for student profiles`);
   try {
-    const [rows] = await connection.query(
-      `SELECT  Name, profilepic, student_id FROM student WHERE teacher_id = '1'`
-    );
+    console.log(`userID: ${req.session.userID}`);
+    const connect = await connection.getConnection();
+    const queryString = `SELECT  Name, profilepic, student_id FROM student WHERE teacher_id = ${req.session.userID}`;
+    const [rows] = await connect.query(queryString);
     console.log(rows, new Date().toISOString());
     res.send(rows);
   } catch (error) {
@@ -16,18 +16,17 @@ const getAllProfiles = async (req, res) => {
 };
 
 const getAllProgress = async (req, res) => {
-  console.log("Received a GET request to api/students/ for student profiles");
-
   try {
-    const [rows] = await connection.query(
-      `SELECT 
-      COUNT(date_completed) 'Total', s.Name, date_completed, project_id, p.student_id
-  FROM
-      progress_history p
-          JOIN
-      student s ON p.student_id = s.student_id
-  GROUP BY s.Name`
-    );
+    const connect = await connection.getConnection();
+    const queryString = `SELECT 
+    COUNT(date_completed) 'Total', s.Name, date_completed, project_id, p.student_id
+    FROM
+    progress_history p
+        JOIN
+    student s ON p.student_id = s.student_id where project_id <= 15 AND teacher_id = '${req.session.userID}'
+    GROUP BY s.Name`;
+    const [rows] = await connect.query(queryString);
+
     console.log(rows, new Date().toISOString());
     res.send(rows);
   } catch (error) {
