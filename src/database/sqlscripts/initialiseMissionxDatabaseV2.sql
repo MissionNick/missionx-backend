913272682636e7cff10758ccf6ce7f2ba6da79ec
Brandon Trump <brandontrump101@gmail.com>
2022-07-25 05:34:31 +1200

    added a new insert script for help requests

--- src/database/sqlscripts/initialiseMissionxDatabaseV2.sql
+++ src/database/sqlscripts/initialiseMissionxDatabaseV2.sql
@@ -195,6 +195,8 @@ INSERT INTO teacher (name, email, password, school, profilepic, date_of_birth, c
 
 INSERT INTO student (teacher_id,name, email, password, school, profilepic, date_of_birth, contact_number, course) VALUES( '1', 'Groot', 'Groot@marvel.com', '$2b$10$qvcZHjfWTQDti7RPW9dOU.U4HvkMwwmh0mkNcewY3ccEbeEI3lyoi', 'Marvel Academy', 'https://cdn.filestackcontent.com/CjZlRoDdQcaiL0DLwMRi', '1960-11-01', '55-555-5555', '1'), ( '1', 'Spiderman', 'peter.parker@marvel.com', '$2b$10$zgs8btmO2hqV.HrXUZZvM.h/lbDyRYSORNMf9O.wEThcS0FN8k65u', 'Marvel Academy', 'https://cdn.filestackcontent.com/L2EqDKS2RFa4E3eOPYeq', '2001-08-10', '55-555-5555', '2');
 
+INSERT INTO help_request (request_id,student_id, date_created, done) VALUES( '1', '1', '2021-11-20 13:45:00', null)
+
 INSERT INTO `progress_history` (`student_id`, `project_id`, `date_started`, `date_submitted`, `date_completed`, `submission`) VALUES ('1','1','2021-10-20','2021-11-01','2021-11-20','https//studentsub.sub@edu.co');
 
 INSERT INTO `progress_history` (`student_id`, `project_id`) VALUES (1,3),(1,4),(1,3),(1,10),(1,20),(1,21),(1,22),(1,23),(1,30),(1,31),(1,32),(1,34),(1,35),(1,50),(1,51), (2,11),(2,12),(2,13),(2,5),(2,6),(2,34),(2,35),(2,36),(2,37),(2,41),(2,42),(2,59);
